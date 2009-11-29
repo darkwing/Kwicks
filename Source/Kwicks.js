a0305cbf844094c7254b09b08ebe31cdb3563059
David Walsh <davidwalsh83@gmail.com>
2009-11-29 14:06:37 -0600

    Updating to 1.1

--- Source/Kwicks.js
+++ Source/Kwicks.js
@@ -1,11 +1,19 @@
 /*
-Script: Kwicks
+---
+description:     Kwicks
 
-License: MIT-style license.
+authors:
+  - David Walsh (http://davidwalsh.name)
 
-Copyright: Copyright (c) 2007-2009 [David Walsh](http://davidwalsh.name/).
+license:
+  - MIT-style license
 
-Author: David Walsh (http://davidwalsh.name)
+requires:
+  core/1.2.1:   '*'
+
+provides:
+  - Kwicks
+...
 */
 var Kwicks = new Class({
 
